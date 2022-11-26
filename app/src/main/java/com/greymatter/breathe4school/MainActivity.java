0c04b27fab1ca7047cbbaf2b866069fbc98622c8
ajay <>
2022-11-26 12:40:38 +0530

    implementing Registration screen

--- app/src/main/java/com/greymatter/breathe4school/MainActivity.java
+++ app/src/main/java/com/greymatter/breathe4school/MainActivity.java
@@ -2,13 +2,20 @@ package com.greymatter.breathe4school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity=MainActivity.this;
+        getSupportActionBar().hide();
+        Intent intent = new Intent(activity, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
