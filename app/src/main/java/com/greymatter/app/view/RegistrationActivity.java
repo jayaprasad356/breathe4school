f7aa2c45feb4f1674fb87e5863156edc2bed7440
jayaprasad356 <jayaprasad356@gmail.com>
2022-11-26 19:20:19 +0530

    design done

--- app/src/main/java/com/greymatter/app/view/RegistrationActivity.java
+++ app/src/main/java/com/greymatter/app/view/RegistrationActivity.java
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.greymatter.app.R;
 
 public class RegistrationActivity extends AppCompatActivity {
     EditText edUniqueId;
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         edUniqueId = findViewById(R.id.edUniqueId);
+        btnContinue = findViewById(R.id.btnContinue);
 
         edUniqueId.addTextChangedListener(new TextWatcher() {
             @Override
@@ -31,6 +34,17 @@ public class RegistrationActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
+                if (!editable.toString().equals("")){
+                    btnContinue.setEnabled(true);
+                    btnContinue.setBackgroundTintList(getResources().getColorStateList(R.color.btncolor));
+
+
+                }
+                else {
+                    btnContinue.setEnabled(false);
+                    btnContinue.setBackgroundTintList(getResources().getColorStateList(R.color.btncolor));
+
+                }
 
             }
         });
