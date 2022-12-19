f68418308d45ff425d3c93b36015719f42689365
tamil6300 <tamil632000@gmail.com>
2022-12-19 12:45:53 +0530

    Submission Home work

--- app/src/main/java/com/app/b4s/view/HomeWorkManagementFragment.java
+++ app/src/main/java/com/app/b4s/view/HomeWorkManagementFragment.java
@@ -8,8 +8,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.b4s.R;
 import com.app.b4s.adapter.HomeWorkSubjectAdapter;
@@ -26,6 +30,7 @@ public class HomeWorkManagementFragment extends Fragment {
     TabLayout tabLayout;
     ViewPager viewPager;
     ViewPagerAdapter viewPagerAdapter;
+    TextView tvSortby;
 
 
     public HomeWorkManagementFragment() {
@@ -39,6 +44,29 @@ public class HomeWorkManagementFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home_work_management, container, false);
 
         rvSubject = root.findViewById(R.id.rvSubject);
+        tvSortby = root.findViewById(R.id.tvSortby);
+
+
+        tvSortby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Initializing the popup menu and giving the reference as current context
+                PopupMenu popupMenu = new PopupMenu(getActivity(), tvSortby);
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        // Toast message on menu item clicked
+                        Toast.makeText(getActivity(), "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                // Showing the popup menu
+                popupMenu.show();
+            }
+        });
 
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
